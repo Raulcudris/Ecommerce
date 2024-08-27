e11e54abb34653b4501b94bf93dca84cadb1ff46
RaulCudris <raulcudriz@hotmail.com>
2024-08-27 10:49:28 -0500

    Implementacion de todos los servicios Eureka registrado

--- orderservice/src/main/java/com/makiia/orderservice/OrderserviceApplication.java
+++ orderservice/src/main/java/com/makiia/orderservice/OrderserviceApplication.java
@@ -1,9 +1,10 @@
 package com.makiia.orderservice;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderserviceApplication {
 
 	public static void main(String[] args) {
