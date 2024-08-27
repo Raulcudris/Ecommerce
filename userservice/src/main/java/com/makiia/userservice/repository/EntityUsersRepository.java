22e781f1653c194d516208a43f1d95c1fa7a2fa7
RaulCudris <raulcudriz@hotmail.com>
2024-08-27 15:43:00 -0500

    Implementacion de user para la autenticacion

--- userservice/src/main/java/com/makiia/userservice/repository/EntityUsersRepository.java
+++ userservice/src/main/java/com/makiia/userservice/repository/EntityUsersRepository.java
@@ -2,11 +2,15 @@ package com.makiia.userservice.repository;
 
 import com.makiia.userservice.entity.EntityUsers;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface EntityUsersRepository extends JpaRepository<EntityUsers, Integer> {
-    Optional<EntityUsers> findByUserName(String username);
+    String FILTER_USERNAME_QUERY = "SELECT c FROM EntityUsers c  WHERE c.username  = :username";
+    @Query(value = FILTER_USERNAME_QUERY)
+    Optional<EntityUsers> findByUserName(@Param("username")  String username);
 }
