a4a8a8d8cf3165d10532aa661161967db0a07300
RaulCudris <raulcudriz@hotmail.com>
2024-08-28 11:40:18 -0500

    Añadiendo el RestTemplateConfig

--- gatewayservice/src/main/java/com/makiia/gatewayservice/dto/RequestDto.java
+++ gatewayservice/src/main/java/com/makiia/gatewayservice/dto/RequestDto.java
@@ -9,32 +9,4 @@ import lombok.NoArgsConstructor;
 public class RequestDto {
     private String path;
     private String method;
-
-    // Constructor con parámetros
-    /*public RequestDto(String path, String method) {
-        this.path = path;
-        this.method = method;
-    }
-
-    // Getters y Setters
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public String getMethod() {
-        return method;
-    }
-
-    public void setMethod(String method) {
-        this.method = method;
-    }
-
-    // Constructor por defecto (opcional)
-    public RequestDto() {}
-    */
-
 }
