0f675a9025028fad2fbb695c98b752442eaa0a03
RaulCudris <raulcudriz@hotmail.com>
2024-08-29 12:50:41 -0500

    Primera modificacion en el microservicio productservices

--- productservice/src/main/java/com/makiia/productservice/controller/ProductController.java
+++ productservice/src/main/java/com/makiia/productservice/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.makiia.productservice.service.ProductsService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,12 +17,14 @@ public class ProductController {
     ProductsService productsService;
 
     @GetMapping("getall")
+    @PreAuthorize("hasRole('admin')")
     public List<ProductsDto> getAll(){
         return productsService.getAll().stream()
                 .map(this::convertToDto)
                 .collect(Collectors.toList());
     }
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('admin')")
     public ResponseEntity<ProductsDto> getById(@PathVariable("id") Integer id){
         Products product = productsService.getProductById(id);
         if (product == null) {
@@ -30,6 +33,7 @@ public class ProductController {
         return ResponseEntity.ok(convertToDto(product));
     }
     @PostMapping("create")
+    @PreAuthorize("hasRole('admin')")
     public ResponseEntity<ProductsDto> save(@RequestBody ProductsDto productDto){
         Products product = convertToEntity(productDto);
         Products savedProduct = productsService.save(product);
--- productservice/src/main/java/com/makiia/productservice/jwt/JwtAuthenticationFilter.java
+++ productservice/src/main/java/com/makiia/productservice/jwt/JwtAuthenticationFilter.java
@@ -1,6 +1,4 @@
 package com.makiia.productservice.jwt;
-
-
 import io.jsonwebtoken.Claims;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -11,39 +9,43 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
-
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
         try {
             String token = request.getHeader("Authorization");
-
-            if (StringUtils.hasText(token) && token.startsWith("Bearer ")) {
+            if(StringUtils.hasText(token) && token.startsWith("Bearer ")){
                 Claims claims = jwtUtil.getClaims(token.substring(7));
+                if (claims != null) {
+                    String username = claims.getSubject();
+                    List<String> roles = claims.get("roles", List.class);
 
-                SimpleGrantedAuthority authority = new SimpleGrantedAuthority(claims.getIssuer());
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                        claims.getSubject(), null, Collections.singleton(authority));
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    List<SimpleGrantedAuthority> authorities = roles.stream()
+                            .map(SimpleGrantedAuthority::new)
+                            .collect(Collectors.toList());
 
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                            username, null, authorities);
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
             }
 
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        }catch (Exception e) {
+            System.out.println("Failed to set user authentication: " + e.getMessage());
         }
         filterChain.doFilter(request, response);
     }
